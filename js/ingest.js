81ac8866911e124d4890f75952a5a81a8cd1f690
Andrew <andrew.vandam@wsj.com>
2016-01-14 10:07:46 -0500

    This mm branch is going to get a lot of fixin.

--- js/ingest.js
+++ js/ingest.js
@@ -4,29 +4,39 @@ var ingest = {
 	},
 	execute: function(specs,error,data){
 		specs.data = data;
-		ingest.startDate(specs);
+		//Dates get restricted before anything else happens, because it's most efficient to just work on that one set
+		ingest.restrictDates(specs);
+		ingest.extractHeders(specs);
 		ingest.unpivot(specs);
 		ingest.nest(specs);
 	},
-	startDate: function(specs){
+	restrictDates: function(specs){
+		//This is the only time that dates get trimmed
 		var startDate = new Date(1992,1,1);
 		specs.data = specs.data.filter(function(d,i){
 			var thisDate = new Date(d.date);
 			return ((thisDate)>=startDate && thisDate <= new Date());
 		})
 	},
-	unpivot: function(specs){
+	extractHeders: function(specs){
+		//Get both dimensions of the data, top and left heders
 		specs.keys = d3.keys(specs.data[0]).filter(function(f){return f != specs.dateCol});
 		specs.flatData = [];
 
-		//Add dates while we're in tehre
+		//At this point, we're always assuming the left heders are dates
 		specs.dates = specs.data.map(function(d,i){return d[specs.dateCol]})
+	},
+	unpivot: function(specs){
+
+		console.log(specs.id)
+		console.log(specs.data.WLD)
 
 		specs.data.forEach(function(dD,iD){
+
 			//Splits the date object to account for weird FRED formatting
 			var dateObj = new Date(dD[specs.dateCol].split('-'));
 
-			//Aggregates from the world bnank
+			//Exclude all known aggegates. The currrent set is mostly from the World Bank. Opposite approach would be to include only known countries
 			var aggregates = [{"iso3":"ARB"},
 				{"iso3":"CEB"},
 				{"iso3":"CSS"},
@@ -58,13 +68,17 @@ var ingest = {
 				{"iso3":"SSF"},
 				{"iso3":"SST"},
 				{"iso3":"UMC"},
-				{"iso3":"WLD"}]
-
-				if(specs.id != "reserves" && specs.id != "reservesWB"){
-					aggregates.push({"iso3":"EMU"})
+		 		{"iso3":"WLD"}
+			]
+				//For charts related to foreign country reserves, we need to include the EMU
+				if(specs.id == "reserves" || specs.id == "reservesWB"){
+					aggregates = aggregates.filter(function(f){return f.iso3!="EMU"})
 				}
+
+				//This is an efficient place to add ranks, for now. Year by year.
 				var rankDomain = [];
 
+				//...to do so, we create a scale that only caculates ranks and returns the right values.
 				aggregates.forEach(function(dAgg,iAgg){
 					specs.keys = specs.keys.filter(function(f){return f!=dAgg.iso3})
 				})
@@ -72,13 +86,13 @@ var ingest = {
 					rankDomain.push(+dD[dC])
 				})
 
-
-
 			rankDomain.sort(function(a,b){return d3.descending(a,b)});
 
 			var rankScale = d3.scale.ordinal()
 				.domain(rankDomain)
 				.range(d3.range(1,rankDomain.length+1))
+
+				 
 
 			specs.keys.forEach(function(dC,iC){
 				var thisValue = dD[dC];
@@ -89,8 +103,9 @@ var ingest = {
 					thisValue = "";
 				}
 
-
-				if(thisValue.search(/([A-Z]|[_,!@#$%^&*();\/"'])/g)==-1){
+				if(dC == "WLD") {
+					console.log(thisValue + " " + specs.id)
+				} else if(thisValue.search(/([A-Z]|[_,!@#$%^&*();\/"'])/g)==-1){
 					specs.flatData.push({
 						date: dD[specs.dateCol],
 						dateObj:dateObj,
